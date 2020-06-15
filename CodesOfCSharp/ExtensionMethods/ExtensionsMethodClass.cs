872406f2b225cf881abe97863fbcbae74f1073b8
DHARSHAN KESAVAN <dharshankesavanpotty@gmail.com>
2020-06-16 00:10:54 +0530

    Extension Method in CSharp

---
+++ CodesOfCSharp/ExtensionMethods/ExtensionsMethodClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class ExtensionsMethodClass
+    {
+        public static bool IsGreaterThan(this int firstValue, int secondValue)
+        {
+            return firstValue > secondValue;
+        }
+        public static int WordCount(this String strToBeCalculated)
+        {
+            return strToBeCalculated.Split(new char[] { ' ', '.', '?' },
+                             StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
---
+++ CodesOfCSharp/ExtensionMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethods; // Importing Extension Methods from Other Class File
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello Extension Methods";            
+            int i = s.WordCount(); // First Extension Method
+            bool result = i.IsGreaterThan(25); // Second Extension Method
+            Console.WriteLine(string.Format("Word Count of String is:- {0}",i));
+            Console.WriteLine(string.Format("Is it exceeding the limit of '25':- {0}",result));
+
+            Console.ReadKey();
+        }
+    }
+}
